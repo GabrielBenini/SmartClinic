57e9e4dfdbdc2c1fdcefd591eb479e68cd6d2078
Gabriel <gabriel.benini2@gmail.com>
2025-11-05 18:21:42 -0300

    Primeiro Commit

---
+++ src/test/java/com/gbenini/SmartClinic/SmartClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbenini.SmartClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
