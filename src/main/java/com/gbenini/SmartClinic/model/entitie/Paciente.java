aba1a4aa0593afe4b208775289e98fc6418becf6
Gabriel <gabriel.benini2@gmail.com>
2025-11-05 18:34:04 -0300

    Criada entidade Paciente

---
+++ src/main/java/com/gbenini/SmartClinic/model/entitie/Paciente.java
@@ -0,0 +1,28 @@
+package com.gbenini.SmartClinic.model.entitie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_pacientes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cpf;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    private String telefone;
+
+}
